343ac55d1c32c59f2512e8ec4cf1410772915add
Eric Mugo <mugoericpg20@gmail.com>
2023-06-01 12:29:27 +0300

    Added save method

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,25 @@ class Dog
         @breed = breed
     end
 
+    def self.create_table
+        DB[:conn].execute("CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)")
+        
+    end
+
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+    end
+
+    def save 
+        if self.id
+            self.update
+        else
+            DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", self.name, self.breed)
+            self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        self
+    end
+
+
+
 end
